6e1d9326035e04d4f247c7c9e9fded67b151b4f6
Saúl Contreras <suulcoder@gmail.com>
2021-11-10 19:14:25 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+function getCurrentDate(){
+    let date = new Date().getDate(); //Current Date
+    let month = new Date().getMonth() + 1; //Current Month
+    let year = new Date().getFullYear(); //Current Year
+    let hours = new Date().getHours(); //Current Hours
+    let min = new Date().getMinutes(); //Current Minutes
+    let sec = new Date().getSeconds(); //Current Seconds
+  
+    return (date + '/' + month + '/' + year + ' ' + hours + ':' + min + ':' + sec);
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The time is:
         </p>
+        <h2>
+          {getCurrentDate()}
+        </h2>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           {"I am a " + getRandomInt(0,100) + "% sure"}
         </a>
       </header>
     </div>
