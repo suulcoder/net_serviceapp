15592ebd6c7e4e7fa5285417eb85759566646b5b
suulcoder <saulcontreras@Sauls-MacBook-Air.local>
2021-11-19 04:11:38 -0600

    restore

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function getCurrentDate(){
+    let date = new Date().getDate(); //Current Date
+    let month = new Date().getMonth() + 1; //Current Month
+    let year = new Date().getFullYear(); //Current Year
+    let hours = new Date().getHours(); //Current Hours
+    let min = new Date().getMinutes(); //Current Minutes
+    let sec = new Date().getSeconds(); //Current Seconds
+  
+    return (date + '/' + month + '/' + year + ' ' + hours + ':' + min + ':' + sec);
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function App() {
+    const [time, setTime] = useState(getCurrentDate());
+    
+    useEffect(() => {
+        const secondsTimer = setInterval(() => {
+           setTime(getCurrentDate());
+        }, 1000);
+        return () => clearInterval(secondsTimer);
+      }, []);
+    
+    
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          The time is:
+        </p>
+        <h2> {time} </h2>
+        <a
+          className="App-link"
+          href="https://uvg.instructure.com/courses/22069/files/3487964?wrap=1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           {"I am " + getRandomInt(0,100) + "% sure"}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
